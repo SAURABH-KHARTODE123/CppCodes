638d385baa290faa0f56890decc14d8a37e4c59e
Saurabh Khartode <96560197+SAURABH-KHARTODE123@users.noreply.github.com>
2023-10-21 12:37:36 +0530

    Add files via upload

---
+++ lab8/ClassInsideClass.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+class Employee{
+	int emp_id;
+	int mob_num;
+	public:
+		void display1(){
+			cout<<"outer class is called"<<endl;
+		}
+		public:
+		class salary{
+			int sal;
+			public:
+				
+				void display(){
+//					cout<<"employee id is"<<emp_id<<endl;
+					cout<<"inner class is called"<<endl;
+				}
+			
+		};
+};
+//void Employee::salary::display1(){
+//	
+//}
+
+
+int main(){
+//	Employee e1;
+//	e1.display1();
+//
+	Employee::salary s1;
+	s1.display();
+
+
+	
+}
---
+++ lab8/constructor_practice.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+class date{
+	int dd,mm,yy;
+	public:
+		void show();
+		date(int,int,int);
+		~date();
+};
+date::date(int d,int m,int y){
+	dd=d;
+	mm=m;
+	yy=y;
+}
+date::~date(){
+	cout<<"destructor is called"<<endl;
+
+}
+
+void date::show(){
+	cout<<"date is"<<dd<<"/"<<mm<<"/"<<yy<<endl;
+}
+int main()
+{
+	date d1(12,2,2023);
+    d1.show();
+//    date d2(d1);
+//    d2.show();
+	
+	
+	
+	return 0;
+}
